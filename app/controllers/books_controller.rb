3c70f3ca7648c3d81175685b6a48a38c95d85f5b
H-Tsuruta311217 <141084685+H-Tsuruta311217@users.noreply.github.com>
2023-08-14 01:10:36 +0000

    [Finish] 削除機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,9 +11,17 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
+  end
+  
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
   end
 
   # 投稿データのストロングパラメータ
