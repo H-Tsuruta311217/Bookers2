318501e7ae76a4bd9afb4f9f1b370d39c51742bb
H-Tsuruta311217 <141084685+H-Tsuruta311217@users.noreply.github.com>
2023-08-15 08:53:01 +0000

    [Add] ユーザー情報更新ページ作成中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,7 @@
 class UsersController < ApplicationController
   before_action :is_matching_login_user, only: [:edit, :update]
 
-  def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
-      flash[:notice] = "You have created book successfully."
-      redirect_to book_path(@book.id)
-    else
-      @books = Book.all
-      render :new
-    end
-  end
+
 
   def show
     @user = User.find(params[:id])
@@ -37,8 +27,9 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :user_introduction)
   end
+
 
   def is_matching_login_user
     user = User.find(params[:id])
