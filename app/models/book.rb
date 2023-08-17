8656be4b26c50dd151db009b9d6f9017b70c62a7
H-Tsuruta311217 <141084685+H-Tsuruta311217@users.noreply.github.com>
2023-08-17 09:26:55 +0000

    [Update] ユーザー情報更新ボタン

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,7 +4,9 @@ class Book < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
-  validates :body, presence: true
+  validates :body,
+   length: { maximum: 200 },
+   presence: true
 
   def get_image
     unless image.attached?
