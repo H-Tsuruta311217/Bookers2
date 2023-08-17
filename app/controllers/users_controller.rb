bf1f4310529bb9e0549ff08e9ace37e691e97b93
H-Tsuruta311217 <141084685+H-Tsuruta311217@users.noreply.github.com>
2023-08-17 02:46:25 +0000

    [Add] ユーザーindexページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,20 +9,25 @@ class UsersController < ApplicationController
     @book = Book.new
   end
 
+  def index
+    @users = User.all
+    @book = Book.new
+  end
+
   def edit
     @user = User.find(params[:id])
   end
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      @user = User.all
+      render :edit
+    end
   end
-
-  def index
-    @users = User.all
-  end
-
 
 
   private
