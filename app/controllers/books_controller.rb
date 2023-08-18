f4c0f637e4d0f37f979a390d515da99de3e5abd1
H-Tsuruta311217 <141084685+H-Tsuruta311217@users.noreply.github.com>
2023-08-18 10:11:36 +0000

    [Update] 他人のユーザー画面

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -36,14 +36,16 @@ class BooksController < ApplicationController
   end
 
   def edit
+    is_matching_login_user
     @book = Book.find(params[:id])
   end
 
   def update
+    is_matching_login_user
     @book = Book.find(params[:id])
     if @book.update(book_params)
-    flash[:notice] = "You have updated book successfully."
-    redirect_to book_path(@book.id)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
     else
       @books = Book.all
       render :edit
@@ -55,6 +57,12 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user.id == current_user.id
+      redirect_to books_path
+    end
   end
 
 end
